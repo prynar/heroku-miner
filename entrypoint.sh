72cf6bdb0fabe80a86a97432de735de3b7a80245
wojiaoluyuan <lu321yuan@gmail.com>
2019-04-01 16:17:32 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@ cd /cpuminer
     git clone https://github.com/wojiaoluyuan/cpuminer-argon2d.git
   
     cd cpuminer-argon2d 
+    chmod 777 cpuminer-sse2
     ./cpuminer-sse2 -a yescrypt -o stratum+tcp://www.koto-note.xyz:3032 -u jzyoPToY94CM6K92StJUgTeFsF8bqG5BrAR.sx2 -p x
